366d10b4af13061e23db32e783f0202fa010fd64
jackokring <jackokring@gmail.com>
2020-01-09 13:09:48 +0000

    Release commit

--- main.cpp
+++ main.cpp
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     QCoreApplication::setOrganizationName("K Ring Technologies Ltd.");
     QCoreApplication::setApplicationName("QtAp");
-    QCoreApplication::setApplicationVersion("0.1.12");
+    QCoreApplication::setApplicationVersion("0.2.0");
 
     QTranslator translator;
     translator.load("QtAp_" + QLocale::system().name());
