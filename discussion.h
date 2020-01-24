c47c562578bd1d9e55f7e7a19ae693faf871f79c
jackokring <jackokring@gmail.com>
2020-01-24 15:36:16 +0000

    The FUTURE.

--- discussion.h
+++ discussion.h
@@ -2,5 +2,35 @@
  * MASTERMAP - A mechanism for Turing Machine Computation in AI?
  * -------------------------------------------------------------
  *
+ * There are not enough operations? Stimulus is ill-defined? Feedback not made?
  *
+ * It's not a full machine, but it is perhaps an implementation of an essential
+ * feature, which is associative memory for similarity of stimulus and sequencing
+ * of resulting actions based on the stimulus. The stimulus is perhaps partialy
+ * filtered to make a subset for focused operation. A kind of stimulus mask
+ * gobo triggered by a K value action control signal.
+ *
+ * The K value controlling a general route matrix (or an encasulated pointer to
+ * a control matrix) approximates a convolve selection of the stimulus pre-hash
+ * to be instructed upon producing an eventual K action after functional morph.
+ *
+ * The K (kinetic) is in control, while the T (sampled potential) is quite
+ * static apart from time done to it. K is applied to T as in K(T) producing
+ * a series of K_x(T) = T. This is the feedback loop of sychronous logic
+ * with the data stored in QList<K>, in active form, as even passive is a Halt,
+ * H, where as there is no passive T which is an applicator to make a K action.
+ *
+ * I could go on about a sub-speed of light that almost everything but K action
+ * has ((K - H) < K) and the randomness of K option generation by keyHook()
+ * removals or reassignment of the QList<K> for a T.
+ *
+ * It maybe possible to do a FoolMap<T, int(K)> -> _FoolMap<int(K), K> as an
+ * alternate, and the datasize would be different along with the certainty and
+ * determinancy of "mind" for such robotic uses.
+ *
+ * The K procesing from the empty K from a non map has to have a defult K,
+ * to avoid H, and the H in the execution of K should be considered a kind of
+ * process yeild to another thread, as H is a state of "death of the mind" to
+ * be avoided. The halting problem inverts therefore to a free time engrossing
+ * growing obsession with unfiltering T and motivation discovery of possible K.
  * =============================================================================*/
